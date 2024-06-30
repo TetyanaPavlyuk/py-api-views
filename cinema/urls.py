27535a75531130df84c4bacf462b517da6dbd620
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-06-30 20:44:22 +0300

    Rename url from cinemas into cinema_halls

--- cinema/urls.py
+++ cinema/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinemas/", cinema_hall_list, name="cinema-list"),
-    path("cinemas/<int:pk>/", cinema_hall_detail, name="cinema-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-list"),
+    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-detail"),
     path("", include(router.urls)),
 ]
 
